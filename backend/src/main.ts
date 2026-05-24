eaaa4b9efbc6982d1efe8b524839385aa24621c1
P Manikandan <manikandanpremkumar@outlook.com>
2026-05-24 20:34:18 +0530

    CORS added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,10 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app =
+    await NestFactory.create(AppModule);
+
   app.enableCors({
-  origin: '*',
-});
-  await app.listen(process.env.PORT ?? 3000);
+    origin: [
+      'http://localhost:5173',
+      'https://no-reboot.vercel.app',
+    ],
+    credentials: true,
+  });
+
+  await app.listen(
+    process.env.PORT ?? 3000,
+  );
 }
+
 bootstrap();
